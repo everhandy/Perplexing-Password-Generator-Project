c19a6dbfd13067cc487ac1decdb19df2ee33221f
Everett Handy <everettdhandy@outlook.com>
2023-09-08 21:41:40 -0400

    Added README.md and more notes to JavaScript file as well as small edits.

---
+++ README.md
@@ -0,0 +1,49 @@
+# Perplexing-Password-Generator-Project
+
+## Description:
+
+This project represents and assignment where I was tasked with creating an application that enables users to generate random passwords based on criteria that they have selected.  This application runs in the user's web browser and features dynamically updated HTML and CSS powered by JavaScript code that I wrote.
+
+## Link to application:
+
+
+
+## Link to screenshot:
+
+
+
+## Installation:
+
+N/A
+
+## Usage:
+
+This project can be used to demonstrate functions within JavaScript such as gathering user inputs and password criteria and generating a selected randomization from different sets of characteristics.
+
+## Credits:
+
+N/A
+
+## License:
+
+MIT License
+
+Copyright (c) [2023] [Everett Handy]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 // Function getPasswordCriteria for getting user's desired password criteria and giving an error message if the length is too short, too long, or not a whole number.
+
 function getPasswordCriteria() {
-  var length = prompt("How long do you want your password to be? (Enter a number from 8 - 128):");
 
-// Sends alert if user input is not a number, an integer, is less than 8, or more than 128.
+  // The acceptance criteria specifies that when clicking the button "generate password", the user will be presented with the following prompts for password criteria:
+ 
+  var includeLowercase = confirm("Would you like your password to include lowercase characters?");
+  var includeUppercase = confirm("Would you like your password to include uppercase characters?");
+  var includeNumeric = confirm("Would you like your password to include numeric characters?");
+  var includeSpecialChars = confirm("Would you like your password to include special characters?");
 
-  if (isNaN(length) || !Number.isInteger(+length) || length < 8 || length > 128) {
-    alert("Password length must be a whole number from 8 to 128.");
-    return null;
-  }
-
-  var includeLowercase = confirm("Include lowercase characters?");
-  var includeUppercase = confirm("Include uppercase characters?");
-  var includeNumeric = confirm("Include numeric characters?");
-  var includeSpecialChars = confirm("Include special characters?");
-
-// Sends alert if no character type is selected
+// Sends alert if no character type is selected and ends the getPasswordCriteria() function with return. The user will have to click generate password again.
 
   if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecialChars) {
-    alert("At least one character type must be selected.");
-    return null;
+    alert("At least one character type must be selected. Please click 'generate password' button to try again.");
+    return;
+  }
+
+// Prompting for length of the password as the acceptance criteria requires with at least 8 and no more than 128 characters.
+
+  var length = prompt("How long do you want your password to be? (Enter a number from 8 - 128):");
+
+// Sends alert if user input is not a number, an integer, is less than 8, or more than 128 and ends the getPasswordCriteria() function with return. The user will have to click generate password again.
+  
+  if (isNaN(length) || !Number.isInteger(+length) || length < 8 || length > 128) {
+    alert("Password length must be a whole number from 8 to 128. Please click 'generate password' button to try again");
+    return;
   }
 
 // returns password criteria for this function that will be stored in the var criteria (line 103)
@@ -43,12 +49,14 @@ function generatePassword(criteria) {
   var password = "";
 
   // Created selectRandomChar function to select a random character from user-selected character types. Used the charAt method to randomize selection from the variable strings.
+ 
   function selectRandomChar(characters) {
     var randomChar = Math.floor(Math.random() * characters.length);
     return characters.charAt(randomChar);
   }
 
   // Ensured at least one character from each selected type will be used in the password.
+
   if (criteria.includeLowercase) {
     password += selectRandomChar(lowercaseChars);
   }
@@ -63,9 +71,11 @@ function generatePassword(criteria) {
   }
 
   // Determined the remaining length
+
   var remainingLength = criteria.length - password.length;
 
   // Combined all selected character types into one pool for the remaining password
+
   var selectedChars = "";
 
   if (criteria.includeLowercase) {
@@ -81,7 +91,8 @@ function generatePassword(criteria) {
     selectedChars += specialChars;
   }
 
-  // Added randomly selected characters from the pool to meet the remaining length
+  // Added randomly selected characters from the selected pool to meet the remaining length
+
   for (let i = 0; i < remainingLength; i++) {
     var randomChars = selectRandomChar(selectedChars);
     password += randomChars;
@@ -91,6 +102,7 @@ function generatePassword(criteria) {
 }
 
 // Function needed to display the generated password
+
 function displayPassword(password) {
   var passwordText = document.getElementById("password");
   passwordText.value = password;
@@ -99,6 +111,7 @@ function displayPassword(password) {
 var generateButton = document.getElementById("generate");
 
 // Event listener for the "Generate Password" button
+
 generateButton.addEventListener("click", function () {
   var criteria = getPasswordCriteria();
   if (criteria) {
